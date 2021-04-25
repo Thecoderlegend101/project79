21f99f3d54b0442c496faaf776e69728da4642d4
Thecoderlegend101 <78315390+Thecoderlegend101@users.noreply.github.com>
2021-04-25 11:32:28 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+var menulist = ['Chicken Tandoori Pizza', 
+'Deluxe Veggie Pizza', 
+'Paneer Tikka Pizza', 
+'Veg Extravaganza Pizza', 
+'Veg Supreme Pizza']
+function getmenu() {
+    var htmldat;
+    htmldat = "<ol id='menlist'>"
+    menulist.sort()
+    for (var i = 0; i < menulist.length; i++) {
+        htmldat = htmldat + '<li>' + menulist[i] + '</li>'
+    }
+    htmldat= htmldat + '</ol>'
+    document.getElementById('list1place').innerHTML = htmldat;
+    
+
+}
+function addmen() {
+    var htmldata;
+    var iteme = document.getElementById('add_item').value;
+    menulist.push(iteme)
+    menulist.sort()
+    htmldata = '<section class="cards">'
+    for (k = 0; k < menulist.length; k++) {
+    
+            htmldata = htmldata + '<div style="background-color: wheat;flex: 0 0 30%;box-sizing: border-box;color:black;margin: 0.5rem .18em;align-items: center;font-size: medium;">' 
+            + '<img src="images/pizzaImg.png"  width="100px", height="80px">' + menulist[k] + '</div>'
+    }
+    htmldata = htmldata + '</section'
+    document.getElementById('addmen').innerHTML = htmldata;
+}
+
+
+
+
